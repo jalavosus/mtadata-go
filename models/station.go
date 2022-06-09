bddbabfea1579f5708564394c3ad35e97f50fc27
James Alavosus <alavosus.james@gmail.com>
2022-06-09 19:23:05 -0400

    models: refactor imports from previous refactors

--- models/station.go
+++ models/station.go
@@ -11,24 +11,24 @@ import (
 
 	"github.com/jalavosus/mtadata/internal/database/dialectors"
 	"github.com/jalavosus/mtadata/internal/utils"
-	"github.com/jalavosus/mtadata/models/borough"
-	"github.com/jalavosus/mtadata/models/division"
+	"github.com/jalavosus/mtadata/models/boroughs"
+	"github.com/jalavosus/mtadata/models/divisions"
 	"github.com/jalavosus/mtadata/models/routes"
-	"github.com/jalavosus/mtadata/models/structure"
+	"github.com/jalavosus/mtadata/models/structures"
 )
 
 type Station struct {
-	DirectionLabels DirectionLabels     `json:"direction_labels" yaml:"direction_labels" gorm:"type:direction_labels"`
-	GtfsStopId      string              `json:"gtfs_stop_id" yaml:"gtfs_stop_id" gorm:"primaryKey"`
-	StopName        string              `json:"stop_name" yaml:"stop_name" gorm:"type:text"`
-	Line            string              `json:"line" yaml:"line" gorm:"type:text"`
-	Division        division.Division   `json:"division" yaml:"division" gorm:"index;type:division"`
-	Borough         borough.Borough     `json:"borough" yaml:"borough" gorm:"index;type:borough"`
-	Structure       structure.Structure `json:"structure" yaml:"structure" gorm:"type:structure"`
-	DaytimeRoutes   routes.Routes       `json:"daytime_routes" yaml:"daytime_routes" gorm:"type:route[]"`
-	GtfsLocation    GtfsLocation        `json:"gtfs_location" yaml:"gtfs_location" gorm:"type:gtfs_location"`
-	StationId       int                 `json:"station_id" yaml:"station_id" gorm:"type:smallint;index"`
-	ComplexId       int                 `json:"complex_id" yaml:"complex_id" gorm:"type:smallint"`
+	DirectionLabels DirectionLabels      `json:"direction_labels" yaml:"direction_labels" gorm:"type:direction_labels"`
+	GtfsStopId      string               `json:"gtfs_stop_id" yaml:"gtfs_stop_id" gorm:"primaryKey"`
+	StopName        string               `json:"stop_name" yaml:"stop_name" gorm:"type:text"`
+	Line            string               `json:"line" yaml:"line" gorm:"type:text"`
+	Division        divisions.Division   `json:"division" yaml:"division" gorm:"index;type:division"`
+	Borough         boroughs.Borough     `json:"borough" yaml:"borough" gorm:"index;type:borough"`
+	Structure       structures.Structure `json:"structure" yaml:"structure" gorm:"type:structure"`
+	DaytimeRoutes   routes.Routes        `json:"daytime_routes" yaml:"daytime_routes" gorm:"type:route[]"`
+	GtfsLocation    GtfsLocation         `json:"gtfs_location" yaml:"gtfs_location" gorm:"type:gtfs_location"`
+	StationId       int                  `json:"station_id" yaml:"station_id" gorm:"type:smallint;index"`
+	ComplexId       int                  `json:"complex_id" yaml:"complex_id" gorm:"type:smallint"`
 }
 
 func (Station) GormDataType() string {
@@ -98,7 +98,7 @@ func (s *Stations) Scan(value any) error {
 			stationMaps[i] = station
 		}
 
-		*s = mapSliceWithCast(stationMaps, stationFromMap)
+		*s = utils.MapSlice(stationMaps, stationFromMap)
 	default:
 		fmt.Printf("%T\n", value)
 	}
--- models/stationcomplex.go
+++ models/stationcomplex.go
@@ -1,13 +1,13 @@
 package models
 
 import (
-	"github.com/jalavosus/mtadata/models/borough"
+	"github.com/jalavosus/mtadata/models/boroughs"
 	"github.com/jalavosus/mtadata/models/routes"
 )
 
 type StationComplex struct {
-	Borough       borough.Borough `json:"borough" yaml:"borough" gorm:"type:borough"`
-	DaytimeRoutes routes.Routes   `json:"daytime_routes" yaml:"daytime_routes" gorm:"type:route[]"`
-	Stations      Stations        `json:"stations" yaml:"stations" gorm:"type:station[]"`
-	ComplexId     int             `json:"complex_id" yaml:"complex_id"`
+	Borough       boroughs.Borough `json:"borough" yaml:"borough" gorm:"type:borough"`
+	DaytimeRoutes routes.Routes    `json:"daytime_routes" yaml:"daytime_routes" gorm:"type:route[]"`
+	Stations      Stations         `json:"stations" yaml:"stations" gorm:"type:station[]"`
+	ComplexId     int              `json:"complex_id" yaml:"complex_id"`
 }
