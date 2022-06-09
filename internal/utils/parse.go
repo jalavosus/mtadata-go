b5b5460883aafbe8dd743e24af75b1456a8bf4d1
James Alavosus <alavosus.james@gmail.com>
2022-06-09 19:20:50 -0400

    internal/utils/parse.go: add docstrings

--- internal/utils/parse.go
+++ internal/utils/parse.go
@@ -4,7 +4,20 @@ import (
 	"strconv"
 )
 
-func ParseFloat(s string) float64 {
+// ParseFloat32 wraps strconv.ParseFloat,
+// panicking if strconv.ParseFloat returns an error.
+func ParseFloat32(s string) float32 {
+	f, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	return float32(f)
+}
+
+// ParseFloat64 wraps strconv.ParseFloat,
+// panicking if strconv.ParseFloat returns an error.
+func ParseFloat64(s string) float64 {
 	f, err := strconv.ParseFloat(s, 64)
 	if err != nil {
 		panic(err)
@@ -13,6 +26,14 @@ func ParseFloat(s string) float64 {
 	return f
 }
 
+// ParseInt wraps strconv.ParseInt,
+// panicking if strconv.ParseInt returns an error.
+func ParseInt(s string) int {
+	return int(ParseInt64(s))
+}
+
+// ParseInt64 wraps strconv.ParseInt,
+// panicking if strconv.ParseInt returns an error.
 func ParseInt64(s string) int64 {
 	n, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
@@ -22,6 +43,8 @@ func ParseInt64(s string) int64 {
 	return n
 }
 
+// ParseUint64 wraps strconv.ParseUint,
+// panicking if strconv.ParseUint returns an error.
 func ParseUint64(s string) uint64 {
 	n, err := strconv.ParseUint(s, 10, 64)
 	if err != nil {
@@ -31,6 +54,8 @@ func ParseUint64(s string) uint64 {
 	return n
 }
 
+// ParseBool wraps strconv.ParseBool,
+// panicking if strconv.ParseBool returns an error.
 func ParseBool(s string) bool {
 	b, err := strconv.ParseBool(s)
 	if err != nil {
