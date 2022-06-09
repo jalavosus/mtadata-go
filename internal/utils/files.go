5f09366813441c78395ced90fb630b50f95b1a46
James Alavosus <alavosus.james@gmail.com>
2022-06-09 19:20:32 -0400

    internal/utils/files.go: add docstrings

--- internal/utils/files.go
+++ internal/utils/files.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -10,14 +11,21 @@ const (
 	openFilePerm      = 0755
 )
 
+// OpenFileWrite wraps os.OpenFile, passing a combo of
+// os.O_RDWR | os.O_CREATE as the flag parameter (enabling file write _or_ creation),
+// and 0755 as the file permission.
 func OpenFileWrite(fp string) (*os.File, error) {
 	return os.OpenFile(fp, openFileWriteFlag, openFilePerm)
 }
 
+// OpenFileRead wraps os.OpenFile, passing os.O_RDONLY
+// and 0755 as the open flag and file permission.
 func OpenFileRead(fp string) (*os.File, error) {
 	return os.OpenFile(fp, openFileReadFlag, openFilePerm)
 }
 
+// ClearFile calls Truncate(0) and Seek(0, 0) on an os.File object,
+// clearing all of its contents.
 func ClearFile(f *os.File) error {
 	if err := f.Truncate(0); err != nil {
 		return err
@@ -28,4 +36,19 @@ func ClearFile(f *os.File) error {
 	}
 
 	return nil
+}
+
+// AbsolutePath wraps filepath.Abs,
+// panicking if filepath.Abs returns an error.
+func AbsolutePath(fp string) string {
+	if filepath.IsAbs(fp) {
+		return fp
+	}
+
+	absFp, err := filepath.Abs(fp)
+	if err != nil {
+		panic(err)
+	}
+
+	return absFp
 }
