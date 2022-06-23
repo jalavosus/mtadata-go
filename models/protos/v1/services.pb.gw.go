a816ad02f58d82a0b4fbc2e3bc8569a7cd264cb6
James Alavosus <alavosus.james@gmail.com>
2022-06-22 20:10:23 -0400

    Update proto definitions

--- models/protos/v1/services.pb.gw.go
+++ models/protos/v1/services.pb.gw.go
@@ -101,6 +101,75 @@ func local_request_MtaDataService_GetStation_0(ctx context.Context, marshaler ru
 
 }
 
+var (
+	filter_MtaDataService_GetStation_1 = &utilities.DoubleArray{Encoding: map[string]int{"gtfs_stop_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
+func request_MtaDataService_GetStation_1(ctx context.Context, marshaler runtime.Marshaler, client MtaDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq StationRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["gtfs_stop_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "gtfs_stop_id")
+	}
+
+	protoReq.GtfsStopId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "gtfs_stop_id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MtaDataService_GetStation_1); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetStation(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_MtaDataService_GetStation_1(ctx context.Context, marshaler runtime.Marshaler, server MtaDataServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq StationRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["gtfs_stop_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "gtfs_stop_id")
+	}
+
+	protoReq.GtfsStopId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "gtfs_stop_id", err)
+	}
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_MtaDataService_GetStation_1); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetStation(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_MtaDataService_GetStations_0(ctx context.Context, marshaler runtime.Marshaler, client MtaDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq StationsQuery
 	var metadata runtime.ServerMetadata
@@ -196,6 +265,75 @@ func local_request_MtaDataService_GetUpcomingTrains_0(ctx context.Context, marsh
 	}
 
 	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_MtaDataService_GetUpcomingTrains_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetUpcomingTrains(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+var (
+	filter_MtaDataService_GetUpcomingTrains_1 = &utilities.DoubleArray{Encoding: map[string]int{"gtfs_stop_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
+func request_MtaDataService_GetUpcomingTrains_1(ctx context.Context, marshaler runtime.Marshaler, client MtaDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpcomingTrainsRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["gtfs_stop_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "gtfs_stop_id")
+	}
+
+	protoReq.GtfsStopId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "gtfs_stop_id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MtaDataService_GetUpcomingTrains_1); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetUpcomingTrains(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_MtaDataService_GetUpcomingTrains_1(ctx context.Context, marshaler runtime.Marshaler, server MtaDataServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpcomingTrainsRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["gtfs_stop_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "gtfs_stop_id")
+	}
+
+	protoReq.GtfsStopId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "gtfs_stop_id", err)
+	}
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_MtaDataService_GetUpcomingTrains_1); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -419,6 +557,26 @@ func RegisterMtaDataServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 
 	})
 
+	mux.Handle("GET", pattern_MtaDataService_GetStation_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_MtaDataService_GetStation_1(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_MtaDataService_GetStation_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_MtaDataService_GetStations_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -456,6 +614,26 @@ func RegisterMtaDataServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 		}
 
 		forward_MtaDataService_GetUpcomingTrains_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_MtaDataService_GetUpcomingTrains_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_MtaDataService_GetUpcomingTrains_1(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_MtaDataService_GetUpcomingTrains_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -640,6 +818,26 @@ func RegisterMtaDataServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 
 	})
 
+	mux.Handle("GET", pattern_MtaDataService_GetStation_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_MtaDataService_GetStation_1(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_MtaDataService_GetStation_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_MtaDataService_GetStations_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -677,6 +875,26 @@ func RegisterMtaDataServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 		}
 
 		forward_MtaDataService_GetUpcomingTrains_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_MtaDataService_GetUpcomingTrains_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_MtaDataService_GetUpcomingTrains_1(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_MtaDataService_GetUpcomingTrains_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -804,11 +1022,15 @@ func RegisterMtaDataServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 }
 
 var (
-	pattern_MtaDataService_GetStation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"v1", "station_id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_MtaDataService_GetStation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "stations", "station_id"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_MtaDataService_GetStation_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "stations", "gtfs_stop_id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_MtaDataService_GetStations_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "stations"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_MtaDataService_GetUpcomingTrains_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "stations", "station_id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_MtaDataService_GetUpcomingTrains_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "stations", "station_id", "upcoming"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_MtaDataService_GetUpcomingTrains_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "stations", "gtfs_stop_id", "upcoming"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_MtaDataService_GetStationComplex_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "complexes", "complex_id"}, "", runtime.AssumeColonVerbOpt(true)))
 
@@ -826,9 +1048,13 @@ var (
 var (
 	forward_MtaDataService_GetStation_0 = runtime.ForwardResponseMessage
 
+	forward_MtaDataService_GetStation_1 = runtime.ForwardResponseMessage
+
 	forward_MtaDataService_GetStations_0 = runtime.ForwardResponseMessage
 
 	forward_MtaDataService_GetUpcomingTrains_0 = runtime.ForwardResponseMessage
+
+	forward_MtaDataService_GetUpcomingTrains_1 = runtime.ForwardResponseMessage
 
 	forward_MtaDataService_GetStationComplex_0 = runtime.ForwardResponseMessage
 
